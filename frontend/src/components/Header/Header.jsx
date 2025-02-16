526b4ebc2f89a7ea3a3cee5342552e2730da5efb
Pavan Saketh <91939881+Pavan-71@users.noreply.github.com>
2025-02-16 11:26:01 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
+import React from 'react';
+import './Header.css';
+
 const Header = () => {
+    
     const scrollToMenu = () => {
-        document.getElementById('explore-menu')?.scrollIntoView({ behavior: 'smooth' });
+        const menuSection = document.getElementById('menu');
+        if (menuSection) {
+            menuSection.scrollIntoView({ behavior: 'smooth' });
+        }
     };
 
     return (
@@ -8,8 +15,11 @@ const Header = () => {
             <div className='header-contents'>
                 <h2>Order your favourite food here</h2>
                 <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                {/* Button with onClick event to trigger scroll */}
                 <button onClick={scrollToMenu}>View Menu</button>
             </div>
         </div>
     );
 };
+
+export default Header;
