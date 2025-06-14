66aa190609a4c46127a26701209296e057c55d24
Pavan Saketh <91939881+Pavan-71@users.noreply.github.com>
2025-06-14 23:47:55 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const currency = "usd";
 const deliveryCharge = 5;
-const frontend_URL = 'http://localhost:5173';
+const frontend_URL = 'https://food-del-frontend-preb.onrender.com';
 
 const placeOrder = async (req, res) => {
 
